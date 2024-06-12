b4f77acee5939dcdd0ff574b6e5917ef5bf2ab83
Oscar <oszczarek@gmail.com>
2024-06-13 00:00:03 +0200

    Added registration form (not complete)

--- src/main/java/com/example/carrentingapp/HelloController.java
+++ src/main/java/com/example/carrentingapp/HelloController.java
@@ -1,11 +1,22 @@
 package com.example.carrentingapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    @FXML
+    private Label loginLabel, registerLabel;
+    @FXML
+    private Button loginButton, registerButton;
+
     @FXML
     protected void test() {
         System.out.println("elo");
+        loginLabel.setVisible(false);
+        registerLabel.setVisible(true);
+        loginButton.setVisible(false);
+        registerButton.setVisible(true);
     }
 }
