a035bfaa43fb5d2a708b1cbc4b4af7cdf8d3d180
Oscar <oszczarek@gmail.com>
2024-06-12 23:33:29 +0200

    Created project and added simple login page

---
+++ src/main/java/com/example/carrentingapp/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.carrentingapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    protected void test() {
+        System.out.println("elo");
+    }
+}
